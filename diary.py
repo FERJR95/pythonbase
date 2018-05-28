139483ac46466c705c396731ef14c5116dd93fc3
william <ferjr95@gmail.com>
2018-05-27 19:04:24 -0500

    commit'eliminar'

--- diary.py
+++ diary.py
@@ -1,63 +1,257 @@
-import sys
-from collections import OrderedDict
+#-*- coding: utf-8 -*-
 from peewee import *
 import datetime
-db=SqliteDatabase('diary.db')#el nombre de la base de datos
+from collections import OrderedDict
+import sys
+import sqlite3
+
+db=SqliteDatabase('diary.db')
 
 class Entry(Model):
-    content= TextField()
-    timestamp=DateTimeField(default=datetime.datetime.now)#permite cojer la fecha y la hora de entrada de este modulo
+    content=TextField()
+    timestamp=DateTimeField(default=datetime.datetime.now)
     class Meta:
         database=db
-#METODOS
+
 def add_entry():
-    """Agregar un registro"""
-    print("Introduzca un registro, Presione Ctrl+z+Enter para terminar")
-    data=sys.stdin.read().strip()#permite seguir trabajando hasta q el usurioa queira parar de hacer
-    #en este caso solo parara cuando aplatemos ctrl+z+enter
-#esto es para guardar en la base de datos el if
+    """AGREGAR UN REG. NUEVO"""
+    print("Introduzca un registro, presione Ctrl+Z y Enter para Terminar")
+    data=sys.stdin.read().strip()
     if data:
-        if input("Desea Guardaar este Registro [Y/N] : ").lower() == 'y':
-              Entry.create(content=data)
-              print("SE HA GUARDADO EXITOSAMENTE")
+        if input("Desea Guardar Este Registro) [Y/N]: ").lower()=='y':
+            Entry.create(content=data)
+            print("SE HA GUARDADO EXITOSAMENTE!")
+
 def view_entries():
-    """Ver registros"""
+    """VER REGISTROS"""
     entries=Entry.select().order_by(Entry.timestamp.desc())
     for entry in entries:
         timestamp=entry.timestamp.strftime('%A %B %D, %Y %I:%M%p')
         print(timestamp)
-        print("_-_-_-_-_-_")
+        print("*************************")
         print(entry.content)
-        print("n----> Ver el siguiente registro")
-        print("q----> Salir del menu")
-
-        next_entry=input("Desea ver la siguiente entrada [n/q]").lower().strip()
-        if next_entry=='q':
+        print("n--> Ver el Siguiente Registro")
+        print("q--> Salir del Menu")
+        next_entry=input("Desea Ver la siguiente entrada? [n/q]").lower().strip()
+        if next_entry == 'q':
             break
-
 def delete_entries():
-    """Eliminar registro"""
+    """ELIMINAR UN REGISTRO"""
+    try:
+        bd=sqlite3.connect("diary.db")
+        cursor=bd.cursor()
 
-menu=OrderedDict([#con este metodo simpre aparce el menu en el orden ingresado
+        sentence="SELECT *,rowid FROM entry;"
+
+        cursor.execute(sentence)
+
+        entres=cursor.fetchall()
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", "", ""))
+        print("|{:^20}|{:^10}|{:^50}|".format("ID", "Contenido", "Fecha", "Rowid"))
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        for id, content, timestamp, rowid in entres:
+            print("|{:^20}|{:^10}|{:^50}|".format(id , content, timestamp, rowid))
+
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", "", ""))
+
+        id_content= input("\nEsciba el Id del Registro a eliminar: ")
+        if not id_content:
+            print("No se escribió nada, intentelo de nuevo")
+            exit()
+
+        sentence="DELETE FROM entry WHERE rowid =?;"
+
+        #ELIMINACION
+        cursor.execute(sentence, [id_content])
+        bd.commit()
+        print("Se Eliminó Correctamente.!")
+
+    except sqlite3.OperationalError as error:
+        print("Error al Abrir:", error)
+
+def search_fordate():
+    """BUSCAR COINCIDENCIAS EN LOS REGISTROS POR FECHA"""
+
+    try:
+        bd=sqlite3.connect("diary.db")
+        cursor=bd.cursor()
+
+        sear=input("Escriba la fecha de lo que desea buscar: ")
+        if not sear:
+            print("Busqueda Invalida")
+            exit()
+
+        sentence="SELECT * FROM entry WHERE timestamp LIKE ?;"
+
+        cursor.execute(sentence, [ "%{}%".format(sear) ])
+
+        entres=cursor.fetchall()
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+        print("|{:^20}|{:^10}|{:^50}|".format("ID", "Contenido", "Fecha"))
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        for id, content, timestamp in entres:
+            print("|{:^20}|{:^10}|{:^50}|".format(id , content, timestamp))
+
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        #print(entres)
+    except sqlite3.OperationalError as error:
+        print("Error al Abrir", error)
+
+
+def search_entries():
+    """BUSCAR COINCIDENCIAS EN LOS REGISTROS"""
+
+    try:
+        bd=sqlite3.connect("diary.db")
+        cursor=bd.cursor()
+
+        sear=input("Escriba lo que desea buscar: ")
+        if not sear:
+            print("Busqueda Invalida")
+            exit()
+
+        sentence="SELECT * FROM entry WHERE content LIKE ?;"
+
+        cursor.execute(sentence, [ "%{}%".format(sear) ])
+
+        entres=cursor.fetchall()
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+        print("|{:^20}|{:^10}|{:^50}|".format("ID", "Contenido", "Fecha"))
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        for id, content, timestamp in entres:
+            print("|{:^20}|{:^10}|{:^50}|".format(id , content, timestamp))
+
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        #print(entres)
+    except sqlite3.OperationalError as error:
+        print("Error al Abrir", error)
+
+def update_entries():
+    """ACTUALIZAR UN REGISTRO"""
+    try:
+        bd=sqlite3.connect("diary.db")
+        cursor=bd.cursor()
+
+        sentence="SELECT *,rowid FROM entry;"
+
+        cursor.execute(sentence)
+
+        entres=cursor.fetchall()
+        print("+{:-<20}+{:-<10}+{:-<50}+{:-<20}+".format("", "", "", ""))
+        print("|{:^20}|{:^10}|{:^50}|{:^20}|".format("ID", "Contenido", "Fecha", "Rowid"))
+        print("+{:-<20}+{:-<10}+{:-<50}+{:-<20}+".format("", "", "", ""))
+
+        for id, content, timestamp, rowid in entres:
+            print("|{:^20}|{:^10}|{:^50}|{:^20}|".format(id , content, timestamp, rowid))
+
+        print("+{:-<20}+{:-<10}+{:-<50}+{:-<20}+".format("", "", "", ""))
+
+        #PETICION
+        id_content= input("\nEsciba el Id del Registro que desea Modificar: ")
+        if not id_content:
+            print("No se escribió nada, intentelo de nuevo")
+            exit()
+
+        #Nuevos Datos
+        content = input("\n Nuevo Registro: ")
+        timestamp = DateTimeField(default=datetime.datetime.now)
+
+
+        #actualizacion
+        sentence = "UPDATE entry SET content = ?, timestamp=timestamp WHERE id = ?;"
+
+        cursor.execute(sentence, [content, id_content])
+        bd.commit()
+        print("Actualizado.!")
+
+    except sqlite3.OperationalError as error:
+        print("Error al Abrir:", error)
+
+
+def search_all():
+    """MOSTRAR TODOS LOS RESULTADOS"""
+    try:
+        bd=sqlite3.connect("diary.db")
+        cursor=bd.cursor()
+        sentence="SELECT * FROM entry;"
+
+        cursor.execute(sentence)
+
+        entres=cursor.fetchall()
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+        print("|{:^20}|{:^10}|{:^50}|".format("ID", "Contenido", "Fecha"))
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        for id, content, timestamp in entres:
+            print("|{:^20}|{:^10}|{:^50}|".format(id , content, timestamp))
+
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        #print(entres)
+    except sqlite3.OperationalError as error:
+        print("Error al Abrir", error)
+
+def search_fordate():
+    """BUSCAR COINCIDENCIAS EN LOS REGISTROS POR FECHA"""
+
+    try:
+        bd=sqlite3.connect("diary.db")
+        cursor=bd.cursor()
+
+        sear=input("Escriba la fecha de lo que desea buscar: ")
+        if not sear:
+            print("Busqueda Invalida")
+            exit()
+
+        sentence="SELECT * FROM entry WHERE timestamp LIKE ?;"
+
+        cursor.execute(sentence, [ "%{}%".format(sear) ])
+
+        entres=cursor.fetchall()
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+        print("|{:^20}|{:^10}|{:^50}|".format("ID", "Contenido", "Fecha"))
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        for id, content, timestamp in entres:
+            print("|{:^20}|{:^10}|{:^50}|".format(id , content, timestamp))
+
+        print("+{:-<20}+{:-<10}+{:-<50}+".format("", "", ""))
+
+        #print(entres)
+    except sqlite3.OperationalError as error:
+        print("Error al Abrir", error)
+
+
+menu=OrderedDict([
     ('a',add_entry),
     ('v',view_entries),
+    ('s',search_entries),
+    ('*',search_all),
+    ('d',delete_entries),
+    ('bf', search_fordate),
+    ('u',update_entries),
 ])
 
 def menu_loop():
     """Mostrar opciones"""
     choice=None
-    while choice != 'x':#mientras la opcion q capture sea diferente de x se repite si aplasta x se sale
-        print("Presiona 'x' para salir ")
-        for key, value in menu.items():#nos permite corrernos cada uno de los elementos q nos manden nuestros elementos
-            print('{}---->{}'.format(key,value.__doc__))
-        choice=input("Escoja Opcion : ").lower().strip()#el strip permite eliminar los espacios o letras q se ponga asi''
-        #se captura lo q el ususario escoja y ademas se manda el comentario
-        #vamos a evaluar si nuestra opcion esta dentro de nuestro menu
+    while choice!='x':
+        print("Presiona 'x' para salir")
+        for key, value in menu.items(): #nos permite correr por cada uno de los elementos
+            print('{}-->{}'.format(key,value.__doc__))
+        choice=input("Elija una opcion: ").lower().strip()#strip elimina espacios o caracteres no deseados
         if choice in menu:
             menu[choice]()
-def init(): #metodo propio para q se conecte
-        db.connect()
-        db.create_tables([Entry],safe=True)#se crea tablas
+
+
+def init():
+    db.connect()
+    db.create_tables([Entry],safe=True)
 if __name__ == '__main__':
-    init()#llama al metodo
+    init()
     menu_loop()
